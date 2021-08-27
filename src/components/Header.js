cf4687dbdca39ee2fdbfea0d9650a3174cf5dc89
molzania <molzania_1507@yahoo.co.id>
2021-08-27 14:59:32 +0700

    commit on 'main'

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Button} from 'react-bootstrap';
+import { Link }from 'react-router-dom';
+import './Header.css';
+import logo from './img/logo.png';
+
+function Header() {
+  return (
+    <div className="Navbar">
+      <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      <img src={logo} alt="" width="50" height="50" className="d-inline-block align-text-top"/>
+    <span><Link to="/">Tugas 17</Link></span>
+    </a>
+  </div>
+  <div className="buttons">
+      <Button>Login</Button>
+      <button>Sign Up</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+      <div className="Home">
+          
+</div>
+    );
+  }
+  
+  export default Home;

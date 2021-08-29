6fd73af1b7e37879514ee7ec885556328dbce362
molzania <molzania_1507@yahoo.co.id>
2021-08-29 15:20:41 +0700

    commit on 'main'

--- src/pages/User.js
+++ src/pages/User.js
@@ -4,23 +4,25 @@ import Video from './video/video.mp4'
 
 function User() {
 return(
-<div>
+  <>
+<div className="video-background">
 <div className="overlay"></div>
-<video playsInline autoPlay muted="muted" loop="loop"/>
-  <source src={Video} type="video/mp4"/>
+<video playsInline autoPlay muted="muted" loop="loop" src={Video} type="video/mp4"/>
 <div className="container h-100">
   <div className="d-flex h-100 text-center align-items-center">
     <div className="w-100 text-white">
-      <h1 className="display-3">User</h1>
-      <p className="lead mb-0">Selamat Menikmati Filmnya!</p>
+      <h1 className="display-3">Laman User</h1>
+      <p className="lead mb-0">Selamat Menikmati Filmnya</p>
     </div>
   </div>
 </div>
 </div>
+</>
+
+
+
 
 )
-
-
 }
 
 export default User;

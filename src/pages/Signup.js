98abec06a0167fa8e5237576de9b4dfd538e48e2
molzania <molzania_1507@yahoo.co.id>
2021-08-29 14:14:24 +0700

    commit on 'main'

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -10,11 +10,22 @@ function SignUp() {
         setFields({
             ...fields,
             [event.target.name]: event.target.value,
-            BirthDate: event.target.value
         }
         )
     }
     const history = useHistory(); 
+    const handleSubmitSignup = (event) => {
+        event.preventDefault();
+        console.log(fields);
+        alert(`Your Data is \n Full Name: ${fields.FullName} \n Username: ${fields.Username} \n Email: ${fields.Email}` )
+        history.push("/login");
+    }
+
+    function validateFormSignup(){
+        return (fields.FullName.length > 0 && fields.Username.length > 0
+            && fields.Email.length > 0 && fields.Password.length > 0 )
+    }
+
 
 
         return (
@@ -27,22 +38,23 @@ function SignUp() {
                <input type="text" className="form-control" placeholder="Full-Name" name="FullName" value={fields.FullName} onChange={handleChangeSignup}/>
            </div>
 
+
            <div className="form-register">
                <label>Username:</label>
-               <input type="text" className="form-control" placeholder="Username" name="username" value={fields.Username} onChange={handleChangeSignup}/>
+               <input type="text" className="form-control" placeholder="Username" name="Username" value={fields.Username} onChange={handleChangeSignup}/>
            </div>
 
            <div className="form-register">
                     <label>Email:</label>
-                    <input type="email" className="form-control" placeholder="Email" name="email" value={fields.Email} onChange={handleChangeSignup}/>
+                    <input type="email" className="form-control" placeholder="Email" name="Email" value={fields.Email} onChange={handleChangeSignup}/>
                 </div>
 
                 <div className="form-register">
                     <label>Password:</label>
-                    <input type="password" className="form-control" placeholder="Enter password" name="password"value={fields.Password} onChange={handleChangeSignup} />
+                    <input type="password" className="form-control" placeholder="Enter password" name="Password" value={fields.Password} onChange={handleChangeSignup} />
                 </div>
 
-                <button type="submit" className="sign-up">Sign Up</button>
+                <button type="submit" className="sign-up" onClick={handleSubmitSignup} disabled={!validateFormSignup}>Sign Up</button>
                 <p className="forgot-password">
                     Sudah daftar <Link to="/login">Log in?</Link>
                 </p>
